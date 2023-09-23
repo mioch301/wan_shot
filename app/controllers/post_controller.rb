50a9e60e72ef9b204f407aad91ba7364219a34ae
EC2 Default User <ec2-user@ip-172-31-45-180.ap-northeast-1.compute.internal>
2023-09-23 06:56:44 +0000

    test deploy

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,28 @@
+class PostController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  
+  # 投稿データの保存
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def index
+    @posts =Post.all 
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :image, :caption)
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "5af355f482d14e1184c50d3964066880.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
